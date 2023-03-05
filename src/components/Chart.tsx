b9b0477a3b1f5339eb1f4fcb1c551e900520f6c6
TheGreatGildo <scupytrooples@mail.alchemix.fi>
2023-03-05 17:04:15 +0900

    hi

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -3,7 +3,7 @@ import { FC } from "react"
 export const Chart: FC = () => {
   return (
     <div className="flex h-96 items-center justify-center bg-slate-300">
-      Chart
+      Chart .
     </div>
   )
 }
